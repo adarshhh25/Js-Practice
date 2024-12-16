c3d8c06bba41022641bbbf1afd287da3ebea41d8
adarshhh25 <adarshjha4425@gmail.com>
2024-12-16 05:37:15 +0000

    Practice

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,5 @@
+const x = 1
+let y = "234"
+var z = adarsh
+
+console.table([x, y, z])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  
+
+// alert("hello")
+//  we are using node.js , not  browser
+
+let name = "om" //string
+let x = 10  //int
+let isLogged = true // boolean
+
+
+
+//null => it's a standlone value
+
+//undefined  => it means value is not assigned
+
+// symbol => unique value
+
+//object
+ console.log(typeof name);
+ console.log(typeof null) //object
+ console.log(typeof 
+    undefined) //undefined
