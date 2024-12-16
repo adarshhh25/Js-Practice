0751c314cfe2731ae59bf87a33fa4316da98c74b
adarshhh25 <adarshjha4425@gmail.com>
2024-12-16 06:21:38 +0000

    Conversions in JS

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,42 @@
+let score1 = "33"
+console.log(typeof score1)
+let newVal1 = Number(score1)
+console.log(typeof newVal1)
+console.log(newVal1)
+console.log();
+
+let score2 = "33abc"
+console.log(typeof score2)
+let newVal2 = Number(score2)
+console.log(typeof newVal2 )
+console.log(newVal2)
+
+console.log()
+
+let score3 = null
+console.log(typeof score3)
+let newVal3 = Number(score2)
+console.log(typeof newVal3 )
+console.log(newVal3)
+
+console.log()
+
+let score4 = undefined
+console.log(typeof score4)
+let newVal4 = Number(score4)
+console.log(typeof newVal4 )
+console.log(newVal4)
+
+console.log()
+
+let score5 = true
+console.log(typeof score5)
+let newVal5 = Number(score5)
+console.log(typeof newVal5 )
+console.log(newVal5)
+
+console.log()
+
+let x ;
+let y = Boolean(x)
+console.log(y)
