3efcd6493f86e269f43f39a40e08de31f5866a42
adarshhh25 <adarshjha4425@gmail.com>
2024-12-16 06:49:12 +0000

    operations in JS

---
+++ .devcontainer/01_basics/04_operations.js
@@ -0,0 +1,24 @@
+let Val = 3;
+let negVal = -Val;
+
+let x = 123
+let y = 456
+let z = x+y
+console.log(z)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2+ 2);
+console.log(1+2+"3");
+
+let a = 3;
+let b = a++;
+console.log(a);
+console.log(b);
+
+let c = 5;
+let d = ++c;
+console.log(c);
+console.log(d);
+
+
