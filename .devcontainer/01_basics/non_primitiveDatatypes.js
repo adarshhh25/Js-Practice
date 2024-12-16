5253eb8ef527f00a06b91e3845ccd44f1cd2755d
adarshhh25 <adarshjha4425@gmail.com>
2024-12-16 09:16:13 +0000

    Summry of Data-types

---
+++ .devcontainer/01_basics/non_primitiveDatatypes.js
@@ -0,0 +1,16 @@
+let a = ["adarsh", 12, "123", 45]
+
+console.log(a);
+
+let myObj = {
+    name: "adarsh",
+    age: 19,
+}
+
+console.log(myObj)
+
+let x = function() {
+    console.log("Hello World");
+}
+
+console.log(x)
