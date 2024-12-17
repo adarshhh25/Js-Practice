c166b42a69b13b48dfd3e1f7a71fe88cd1522d68
adarshhh25 <adarshjha4425@gmail.com>
2024-12-17 03:43:44 +0000

    Maths

---
+++ .devcontainer/01_basics/07_numsAndMaths.js
@@ -0,0 +1,17 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100);
+// console.log(balance)
+// console.log(balance.toString())
+
+// const oNum = 123.9956
+// console.log(oNum.toPrecision(3))
+
+// +++++++++++++++++++++++++++++++++++++
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.min(4,4,23,45));
+console.log(Math.max(null,4,5));
