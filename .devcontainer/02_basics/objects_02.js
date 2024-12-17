06b4ffa1700d76c80e9a69402bf1c2c20366adba
adarshhh25 <adarshjha4425@gmail.com>
2024-12-17 12:15:02 +0000

    Object Done

---
+++ .devcontainer/02_basics/objects_02.js
@@ -0,0 +1,51 @@
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        f_name: "adarsh",
+        m_name: "abhiram",
+        l_name: "jha"
+    }
+}
+
+// console.log(regularUser.fullname.f_name)
+
+const obj1 = {
+    1: "a",
+    2: "b",
+    3: "c"
+}
+
+const obj2 = {
+    4: "x",
+    5: "y",
+    6: "z"
+}
+
+// const obj3 = {obj1, obj2}
+
+const obj4 = Object.assign(obj1, obj2)
+
+// console.log(obj3)
+// console.log(obj4)
+// console.log(obj1)
+
+const user = [
+    {
+        id: 1,
+        email : "h@email"
+    },
+    {
+        id: 1,
+        email : "h@email"
+    },
+    {
+        id: 1,
+        email : "h@email"
+    },
+]
+
+// console.log(regularUser);
+// console.log(Object.values(regularUser))
+console.log(Object.entries(regularUser))
+
+console.log(regularUser.hasOwnProperty('email'));
